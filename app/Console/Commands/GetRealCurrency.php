c6d3da027a40374f5f214bc9ed167daf87afcab7
wp-newbiefriendly <locokoks@gmail.com>
2025-10-20 23:11:38 +0200

    Domaci #20.16 - Ubacen API za trenutni kurs EUR,USD - PRAVI COMMIT

---
+++ app/Console/Commands/GetRealCurrency.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class GetRealCurrency extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get-real-currency';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::get('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_D081BnRtbOXZZi6hgVsVggrpwLlJTZc5awSX47Uh&currencies=EUR%2CUSD%2CCAD&base_currency=EUR');
+//        $response = Http::get(env('REAL_CURRENCY_API_URL'));
+        dd($response->json());
+    }
+}
