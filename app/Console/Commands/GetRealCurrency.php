7dcf3a66531f2981f9599f110298fbefefe00d43
milan <milanmastervisacard@gmail.com>
2025-10-22 18:26:42 +0200

    Vezba #21.09 - added command to check currencies and update them

--- app/Console/Commands/GetRealCurrency.php
+++ app/Console/Commands/GetRealCurrency.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\ExchangeRates;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -12,7 +13,7 @@ class GetRealCurrency extends Command
      *
      * @var string
      */
-    protected $signature = 'get-real-currency';
+    protected $signature = 'getrc';
 
     /**
      * The console command description.
@@ -26,12 +27,31 @@ class GetRealCurrency extends Command
      */
     public function handle()
     {
-      $response = Http::get('https://kurs.resenje.org/api/v1/currencies/eur/rates/today'); // Domaci API bez autentifikacije test
+//      $response = Http::get('https://kurs.resenje.org/api/v1/currencies/eur/rates/today'); // Domaci API bez autentifikacije test
 
     //  $response = Http::get(env('REAL_CURRENCY_API_URL')); // Iz ENV fajla
 
-//        $response = Http::get('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_D081BnRtbOXZZi6hgVsVggrpwLlJTZc5awSX47Uh&currencies=EUR%2CUSD%2CCAD&base_currency=EUR');
+//    $response = Http::get('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_D081BnRtbOXZZi6hgVsVggrpwLlJTZc5awSX47Uh&currencies=EUR%2CUSD%2CCAD&base_currency=EUR');
 
-        dd($response->json()['exchange_middle']);
+//        $currencies = ['EUR', 'USD', 'CAD'];
+
+        foreach (ExchangeRates::CURRENCY_LIST as $currency)
+        {
+            $todayCurrency = ExchangeRates::getCurrency($currency);
+            if($todayCurrency != null)
+            {
+                continue;
+            }
+
+            $response = Http::get("https://kurs.resenje.org/api/v1/currencies/$currency/rates/today"); // Domaci API bez autentifikacije test
+            ExchangeRates::create([
+                'currency' => $currency,
+                'value' => $response->json()['exchange_middle']
+            ]);
+
+
+
+            var_dump($response->json()['exchange_middle']);
+        }
     }
 }
