8e1357dee90eecab3147a1b2a864853418f0a331
wp-newbiefriendly <locokoks@gmail.com>
2025-11-04 10:16:08 +0100

    Vezba #24.13 - Order $ OrderItems tabele + modeli

---
+++ app/Models/Orders.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    protected $table = "orders";
+
+    protected $fillable = [
+        'user_id', 'total_price', 'status'
+    ];
+}
